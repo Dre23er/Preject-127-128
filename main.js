5b38a18808a1152c83cacf0eb5d1cac44d3f5e51
Dre23er <79549421+Dre23er@users.noreply.github.com>
2023-04-02 20:38:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+song1 = "";
+song2 = "";
+
+function preload(){
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.position(665,225);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play(){
+    song.play();
+    song.volume(1);
+    song.rate(1);
+}
+
